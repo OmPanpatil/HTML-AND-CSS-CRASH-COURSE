a07d127d8a032ee5924e2d03ae5d7ec32dc2c5cb
Om Panpatil <ompanpatil513@gmail.com>
2024-12-04 16:48:26 +0530

    Started with JS Core

---
+++ JS_Learning/Basic_Learning_js/script.js
@@ -0,0 +1,16 @@
+// Js the language
+// Js the Dom - creating the phase
+
+// JS the language
+
+// Basics, In-Built features : Browser mae alert, prompt, 
+// Functions and console, Variables, Constants
+// Compilers and interpreters
+// Window
+// Types 
+// Conditional statements, Loops, Functions : 6 types of functions
+// Arrays, Objects, Questions related to it
+// Prototypal inheritance,
+// Asynchronous JS
+// ES6 Climax
+// 
