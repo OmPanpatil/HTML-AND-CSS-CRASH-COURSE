fab6d20b2589c4d678ac213b8b3433f6e4951af6
Om Panpatil <ompanpatil513@gmail.com>
2024-12-12 12:02:49 +0530

    Learning JS

--- JS_Learning/Basic_Learning_js/script.js
+++ JS_Learning/Basic_Learning_js/script.js
@@ -3,7 +3,7 @@
 
 // JS the language
 
-// Basics, In-Built features : Browser mae alert, prompt, 
+// Basics, In-Built features :- Browser mae alert, prompt, 
 // Functions and console, Variables, Constants
 // Compilers and interpreters
 // Window
@@ -20,7 +20,20 @@
 // JS The Language :
 // --> Made by Brendan Eigg
 // And to work with the functionality and update and also manage it here is a company known as Ecmascript
+// Naye features lanna, puraane hatana, kharab features sahi karna and overall development of js.
 
+// Js now only have two parts ES5 and ES6 
+// Es5 is known as ES5 and before
+// ES6 is known as ES6 and after
+// ES5 (Var) --> ES6 (Let)
+
+// Difference between JS the language and Js the Dom :
 // Js The Language = Code sikhana, main feature
 // Js the Dom = code likhkar cheezein banana Seekhna
 
+// Basics - file connect karege and load karege script ko, basics of code execution, grammar and understanding of errors.
+
+// In-Built Features : Console, alert, prompt
+// And also contain web Context API.
+
+// Prompt alert ka bhai hai jou ki aek kadam aage hai
---
+++ JS_Learning/Learning_Basics/script.js
@@ -0,0 +1,22 @@
+var a = 12;
+var b = 14;
+
+console.log("Hey");
+
+// Inbuilt Feature : COnsole, alert, prompt
+// Console.log => message, Console.warn => warn, console.error => error
+
+console.log("Om Panpatil")
+
+console.warn("Om")
+
+console.error("O")
+
+// Alert
+
+alert("Hey, How are you! We will catch you in a short time")
+
+// Prompt alert ka bhai hai jou ki aek kadam aage hai
+
+prompt("Tell us your best time")
+
